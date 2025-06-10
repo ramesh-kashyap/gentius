e40b3e92f04c62be5dfe4088aa04f1494684e0b7
Ashutosh <pashutosh563@email.com>
2025-06-10 17:12:16 +0530

    q

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -251,7 +251,7 @@
                                     <div class="swiper-slide" role="group" aria-label="9 / 9"
                                         style="width: 152.833px; margin-right: 30px;">
                                         <div class="icon-box-style4">
-                                            <a href="?a=logout">
+                                            <a href="{{route('logout')}}">
                                                 <div class="icon-wrapper"><svg xmlns="http://www.w3.org/2000/svg"
                                                         viewBox="0 0 24 24" width="48" height="48" id="log-out">
                                                         <path
